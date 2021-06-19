7c6b374f63ea0c278e9005182300f18a9b629b93
Kim <hyunjin1612@gmail.com>
2021-06-19 17:11:51 +0900

    가변길이 인자

--- src/main/kotlin/function/main.kt
+++ src/main/kotlin/function/main.kt
@@ -7,7 +7,19 @@ import function.strings.lastCharProperty
 import java.lang.StringBuilder
 import function.strings.lastChar as last // as 키워드를 사용하면 임포트한 클래스나 함수를 다른 이름으로 부를 수 있다.
 
-fun main() {
+/* varargs
+*   코틀린의 가변 길이 인자(varargs)도 자바와 비슷하다. 타입 뒤에 ...을 붙이는 대신 파라미터 앞에 varargs 변경자를 붙인다.
+*   fun listOf<T>(varargs values: T): List<T> {...}
+*
+*   배열에 들어있는 원소를 가변 길이 인자로 넘길 때
+*   자바에서는 배열을 그냥 넘기면 되지만
+*   코틀린에서는 명시적으로 풀어서 각 원소가 인자로 전달되게 해야 한다.
+*   스프레드 연산자(*)를 사용하면 된다.
+*
+* */
+fun main(args: Array<String>) {
+    val argsList = listOf("args", *args)
+    println(argsList)
     val set = hashSetOf(1, 7, 53)
     val list = arrayListOf(1, 7, 53)
     val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three") // 여기서 to는 특별한 키워드가 아니라 일반함수다.
